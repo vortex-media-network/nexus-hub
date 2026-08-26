8441d90b28edc3e2ed57a26f621dc1bb075b9c81
vortex-media-network <sukhedhaban5@gmail.com>
2026-08-26 12:04:09 +0530

    Create groups.js

---
+++ groups.js
@@ -0,0 +1,45 @@
+const myWhatsAppGroups = [
+  {
+    name: "Tamil Amma Group 1",
+    link: "https://chat.whatsapp.com/DKSS5SXO5y19qOIAVcxkBZ?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  },
+  {
+    name: "Tamil Aunty Group 1",
+    link: "https://chat.whatsapp.com/CauEa4KfnPFB1l1oEt2dBF?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  },
+  {
+    name: "Tamil Amma Group 2",
+    link: "https://chat.whatsapp.com/ENmCjtoWmssKJEwq9rjPGb?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  },
+  {
+    name: "Tamil Aunty Group 2",
+    link: "https://chat.whatsapp.com/Bhfq7PdToX5IHalKjoVqsd?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  },
+  {
+    name: "Tamil Amma Chat",
+    link: "https://chat.whatsapp.com/LauiaRiXCT24I2arLOQGeo?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  },
+  {
+    name: "Tamil Aunty Hub",
+    link: "https://chat.whatsapp.com/LegsrYsosn94ZkEJ42dwAJ?s=cl&p=a&mlu=4",
+    category: "18+",
+    country: "India",
+    language: "Tamil"
+  }
+];
+
