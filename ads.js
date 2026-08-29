a0611e2d246dcea7c106d4d6e62d49611f5c40a6
vortex-media-network <sukhedhaban5@gmail.com>
2026-08-29 12:17:09 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,29 @@
+// DIRECT LINK & SOCIAL BAR CONFIG
+var ADSTERRA_DIRECT_LINK = "https://www.effectivecpmnetwork.com/bwk3i6y31?key=6e1b96d31c0b3e0dcc81cd10c035d3dd";
+
+(function() {
+  var s = document.createElement("script");
+  s.src = "https://pl31003628.profitableratecpmnetwork.com/85/4c/9b/854c9b96d885abc1a70ca7aa7c7af816.js";
+  s.async = true;
+  document.head.appendChild(s);
+})();
+
+function renderBannerAd(containerId) {
+  var box = document.getElementById(containerId);
+  if (!box) return;
+  var iframe = document.createElement('iframe');
+  iframe.style.width = '300px';
+  iframe.style.height = '250px';
+  iframe.style.border = 'none';
+  iframe.scrolling = 'no';
+  box.appendChild(iframe);
+
+  var doc = iframe.contentWindow.document;
+  doc.open();
+  doc.write('<!DOCTYPE html><html><body style="margin:0;padding:0;display:flex;justify-content:center;align-items:center;"><script type="text/javascript">atOptions = {"key":"faf82f0cfce7fb8b734c0e23e96c8ef3","format":"iframe","height":250,"width":300,"params":{}};\x3C/script><script type="text/javascript" src="https://www.highrevenueformat.com/faf82f0cfce7fb8b734c0e23e96c8ef3/invoke.js">\x3C/script></body></html>');
+  doc.close();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderBannerAd('banner-ad-box');
+});
